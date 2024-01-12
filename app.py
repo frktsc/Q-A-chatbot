4ffab136770cb6eb845198c3b44e3d9a68193298
frktsc <ftasc40@gmail.com>
2024-01-11 23:33:41 -0500

    commited

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import streamlit as st
 import google.generativeai as genai
 
 
-genai.configure(api_key=os.getenv("GOOLE_API_KEY"))
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 model=genai.GenerativeModel("gemini-pro")
 chat = model.start_chat()
