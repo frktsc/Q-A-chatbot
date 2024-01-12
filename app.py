2e28e8055e8c041d9c94221eb363e3e98e10b852
frktsc <ftasc40@gmail.com>
2024-01-11 23:08:02 -0500

    commited

--- app.py
+++ app.py
@@ -1,5 +1,29 @@
 from dotenv import load_dotenv
+load_dotenv()
+
+
 import os
 import streamlit as st
+import google.generativeai as genai
 
-load_dotenv(api_key=os.getenv("GOOLE_API_KEY"))
+
+genai.configure(api_key=os.getenv("GOOLE_API_KEY"))
+
+model=genai.GenerativeModel("gemini-pro")
+chat = model.start_chat()
+def get_Response(question):
+    response = chat.send_message(question)
+    return response.text
+
+st.set_page_config(page_title="Q-A CHATBOT")
+
+st.header("Gemini LLM Application")
+
+input=st.text_input("Input: ",key="input")
+
+submit=st.button("Submit")
+
+if submit:
+    response=get_Response(input)
+    st.subheader("the response is")
+    st.write(response)
